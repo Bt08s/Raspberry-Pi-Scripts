182f4c5466d015a59cb2795ceff1e77ef51809bf
Bt08s <68190921+Bt08s@users.noreply.github.com>
2024-02-16 11:42:54 +0300

    Update fix.py

--- Network/fix.py
+++ Network/fix.py
@@ -8,18 +8,5 @@ def enable_wifi():
     os.system("sudo nmcli radio wifi on")
 
 
-def switch_network_manager():
-    try:
-        subprocess.run("sudo systemctl enable NetworkManager", shell=True)
-    except:
-        subprocess.run("sudo apt-get install network-manager -y", shell=True)
-
-    subprocess.run("sudo systemctl stop dhcpcd", shell=True)
-    subprocess.run("sudo systemctl disable dhcpcd", shell=True)
-    subprocess.run("sudo systemctl enable NetworkManager", shell=True)
-    subprocess.run("sudo systemctl start NetworkManager", shell=True)
-
-
 if __name__ == "__main__":
-    switch_network_manager()
     enable_wifi()
